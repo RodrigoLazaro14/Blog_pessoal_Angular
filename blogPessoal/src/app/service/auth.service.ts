6851bfe0071efc43833fb7807595664c83686f1a
unknown <lazaro_db@hotmail.com>
2020-11-04 14:17:51 -0300

    Consumo de API

---
+++ blogPessoal/src/app/service/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  logar(userLogin: UserLogin) {
+    return this.http.post('http://localhost:9000/usuarios/logar', userLogin)
+  } //localhost:9000 por conta do docker, caso o docker não esteja funcionando, acredito que use localhost:8080, será no caso a porta que está usando no sts
+
+  cadastrar(user: User) {
+    return this.http.post('http://localhost:9000/usuarios/cadastrar', user)
+  }
+
+}
